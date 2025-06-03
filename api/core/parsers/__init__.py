a721c43461dff2cc36014174bd9c4248f6ec93e0
h-pgy <hpougy@gmail.com>
2025-06-03 17:12:30 -0300

    adding and parsing details of plano de acao

--- api/core/parsers/__init__.py
+++ api/core/parsers/__init__.py
@@ -1,3 +1,5 @@
 from .plano_acao_basico import ParsePlanoAcaoBasico
+from .plano_acao_detailed import PlanoAcaoDetailed
 
 parse_plano_acao_basico = ParsePlanoAcaoBasico()
+parse_plano_acao_detailed = PlanoAcaoDetailed()
